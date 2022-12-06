bd226325281715598666f92ebefb6b07890b49cc
Caramelcell <caramel_email@icloud.com>
2022-12-06 23:24:48 +0800

    wo shi sha bi

---
+++ day35/main.cc
@@ -0,0 +1,106 @@
+#include <ctype.h>
+#include <math.h>
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::istringstream;
+using std::map;
+using std::string;
+using std::vector;
+
+string FileToString(ifstream&);
+// map<string, int> FreqAllWord(const string&);
+map<string, int> FreqWord(const vector<string>&, const string&);
+double VectorLen(map<string, int>& vec);
+double Similarity(const string& text1, const string& text2);
+
+auto DelPunctuation = [](auto a) {
+  if (!isalpha(a)) a = ' ';
+  return a;
+};
+
+vector<string> function_word = {"the", "at", "are", "where", "when"};
+
+string FileToString(ifstream& file) {
+  string line;
+  string ret = "";
+  while (getline(file, line)) {
+    std::transform(line.begin(), line.end(), line.begin(), DelPunctuation);
+    std::transform(line.begin(), line.end(), line.begin(), tolower);
+    ret += line + " ";
+  }
+  return ret;
+}
+
+// map<string, int> FreqAllWord(const string& text) {
+//   map<string, int> freq_all_words;
+//   string buf;
+//   istringstream iss(text);
+//   while (iss >> buf) {
+//     freq_all_words[buf]++;
+//   }
+//   return freq_all_words;
+// }
+
+map<string, int> FreqWord(const vector<string>& function_word,
+                          const string& text) {
+  map<string, int> freq_words;
+  for (auto i : function_word) {
+    freq_words.insert({i, 0});
+  }
+  string buf;
+  istringstream iss(text);
+  while (iss >> buf) {
+    for (auto word : function_word) {
+      if (buf == word) {
+        freq_words[buf]++;
+      }
+    }
+  }
+  return freq_words;
+}
+
+double VectorLen(map<string, int>& vec) {
+  double ret = 0;
+  for (auto i : vec) {
+    ret += i.second * i.second;
+  }
+  return sqrt(ret);
+}
+
+double Similarity(const string& text1, const string& text2) {
+  map<string, int> freq_word1 = FreqWord(function_word, text1);
+  map<string, int> freq_word2 = FreqWord(function_word, text2);
+  int dotpro;
+  double len;
+  auto i1 = freq_word1.begin();
+  auto i2 = freq_word1.begin();
+  while (i1 != freq_word1.end() && i2 != freq_word2.end()) {
+    dotpro += (i1->second) * (i2->second);
+    ++i1;
+    ++i2;
+  }
+  return dotpro / (VectorLen(freq_word1) * VectorLen(freq_word2));
+}
+
+int main(int argc, char* argv[]) {
+  ifstream file1(argv[1]);
+  ifstream file2(argv[2]);
+
+  string str_file1 = FileToString(file1);
+  string str_file2 = FileToString(file2);
+
+  double result = Similarity(str_file1, str_file2);
+
+  cout << result << endl;
+  return 0;
+}
