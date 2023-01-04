2d43c8d8b345c902a1408258dcd2a8736e666e06
Caramelcell <caramel_email@icloud.com>
2023-01-04 16:42:21 +0800

    leetcode13

--- day49/main.cc
+++ day49/main.cc
@@ -16,50 +16,15 @@ int main() {
       unordered_map<char, int> num = {{'I', 1},   {'V', 5},   {'X', 10},
                                       {'L', 50},  {'C', 100}, {'D', 500},
                                       {'M', 1000}};
-      int pre{0}, cur{1}, ret{0};
+      int pre{0}, ret{0};
       int len = s.size();
-      while (cur < len) {
-        if (s[pre] == 'I' && s[cur] == 'V') {
-          ret += 4;
-          pre += 2;
-          cur += 2;
-          continue;
-        }
-        if (s[pre] == 'I' && s[cur] == 'X') {
-          ret += 9;
-          pre += 2;
-          cur += 2;
-          continue;
-        }
-        if (s[pre] == 'X' && s[cur] == 'L') {
-          ret += 40;
-          pre += 2;
-          cur += 2;
-          continue;
-        }
-        if (s[pre] == 'X' && s[cur] == 'C') {
-          ret += 90;
-          pre += 2;
-          cur += 2;
-          continue;
-        }
-        if (s[pre] == 'C' && s[cur] == 'D') {
-          ret += 400;
-          pre += 2;
-          cur += 2;
-          continue;
-        }
-        if (s[pre] == 'C' && s[cur] == 'M') {
-          ret += 900;
-          pre += 2;
-          cur += 2;
-          continue;
-        }
-        ret += num[s[pre]];
+      while (pre < len) {
+        if (pre < len - 1 && num[s[pre]] < num[s[pre + 1]])
+          ret -= num[s[pre]];
+        else
+          ret += num[s[pre]];
         ++pre;
-        ++cur;
       }
-      if (pre < len) ret += num[s[pre++]];
       return ret;
     }
   };
